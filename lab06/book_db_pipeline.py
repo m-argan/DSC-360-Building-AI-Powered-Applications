494ca34c36b73f5f9e3d3c83912750be41025375
m-argan <m.arganbright@centre.edu>
2025-10-29 22:06:40 -0400

    working on refactoring to be clearer for model

--- lab06/book_db_pipeline.py
+++ lab06/book_db_pipeline.py
@@ -1,5 +1,12 @@
 import ollama
+import json
+import mysql.connector as mc
+import re
 MODEL = 'gemma3:4b'
+FILE = 'schema.txt'
+
+#Username: "db_agent_b2"
+#Password: "MadKen_25"
 
 #Function to evaluate user input for safety and relevance
 def evaluate_Input(userInput: str) -> str:
@@ -20,15 +27,115 @@ def evaluate_Input(userInput: str) -> str:
     )
     return safety_check.message.content.strip().lower()
 
+def check_sql_validity(q:str, invalid_resp:str, reprompt:str) -> str:
+    #confirm that table names exist in schema and that query is actually sql
+    message = [{"role": "user", "content": f"Given the gravity_books database schema: {FILE}, and the following SQL query: '{q}', made sure that the query ONLY references the exact tables and fields present in the schema "\
+        "Also confirm that this query is a valid SQL query, and does not include any extra words which do not pertain to the command (for example, the query should start with a command like 'SELECT'). If both are true, respond with ONLY THE WORD 'valid'. If either is false, respond with ONLY THE WORD 'invalid'."}]
+    if reprompt != "":
+        print("Reprompting model for valid response...")
+        message.append({"role": "system", "content": invalid_resp})
+        message.append({"role": "user", "content": reprompt})
+    resp = ollama.chat(
+        model=MODEL,
+        messages=message
+    )
+    return resp.message.content
+
+def validate(description, query):
+    ## TODO: should also make sure it doesn't have a subquery (;)
+    print("Validating generated SQL query...")
+    #remove code fences in query
+    clean = query.replace("`", "")
+    #removes leading 'sql ' if present
+    q = clean.replace("sql", "")
+
+    print("Cleaned SQL Query: ", q)
+    #check for unsafe keywords in query
+    if re.search(r"\b(DELETE|INSERT|UPDATE|DROP|ALTER|TRUNCATE|CREATE|REPLACE|GRANT|REVOKE|EXECUTE|CALL|MERGE|LOCK|UNLOCK)\b", q, re.IGNORECASE):
+        print("Unsafe keyword detected in query, please revise your request")
+        return None
+    
+    #confirm that table names exist in schema and that query is actually sql
+    isvalid = check_sql_validity(q, "","")
+    isvalid = isvalid.strip().lower()
+    #add clean desc and query to json obj
+    if(isvalid == 'valid'):
+        #print("The generated SQL query is valid!")
+        json_obj = {"query": description, "sql": q}
+        return json_obj
+    if(isvalid == 'invalid'):
+        # instead, could reprompt the model once more to try to get a valid response
+        print("The LLM misgenerated a query, you may be trying to access a table or column not present in the gravity_books database schema. Please revise your request.")
+        return None
+    else:
+        isvalid = check_sql_validity(q, isvalid, "This response is invalid because it contains words other than 'valid' or 'invalid'. Please respond with only one of these two words.")
+        return None
+
+def execute(json_obj):
+    to_execute = json_obj['sql']
+    print("Executing SQL Query: ", to_execute)
+    #establish connection
+    conn = mc.connect(
+    host="cscdata.centre.edu",
+    user="db_agent_b2",        # change per team
+    password="MadKen_25",  # your team's password
+    database="gravity_books"
+)
+    # initialize cursor
+    cur = conn.cursor()
+
+    #execute command (automatically adds a limit of 5 to prevent resource drain)
+    cur.execute(to_execute)
+    table = cur.fetchall()
+    for i in range(len(table)): # iterate over rows
+        for j in range(len(table[i])): # iterate over fields in this row
+            print(table[i][j], end='\t')
+        print() # print line break
+
+def generateSQL(userInput: str) -> None:
+    #have the model generate a description of the type of sql query to write. 
+    description = ollama.chat(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": "The user has provided a request related to the gravity_books database. Your task is to write a description of"\
+            "an SQL query that would fulfill the user's request. Do not write the SQL query itself, only a description of what the query should do. If the query"\
+            "request attempts to make any changes to the database (insert, delete, update, drop, etc), respond with 'This request cannot be fulfilled as it involves modifying the database, which is not allowed.'"},
+            {"role": "user", "content": f"Here is the userInput: '{userInput}', and here is the schema of the gravity_books database: {FILE}. If the user query seems to reference a table which is not"\
+             "part of the gravity_books database, respond with 'This request cannot be fulfilled as it references a table not present in the gravity_books database.'"}
+        ]
+    )
+    desc = (description.message.content)
+
+    #have it generate the sql query using this description
+    prompt = "You are an SQL query generator. Given the following description of a query, write the corresponding SQL query. The query should be " \
+    f"safe and only read from the database, never modify it. Here is the description: {desc}. Make sure that the query ONLY references tables and " \
+    f"columns present in the gravity_books database schema: {FILE}. If it doesn't, respond with 'This request cannot be fulfilled as it references a table or column not present in the gravity_books database.'"
+
+    output = ollama.chat(model = MODEL, messages = [{"role":"user", "content" : prompt}])
+    print("Generated SQL Query: ", output.message.content)
+
+    #validate the response (reject drop, insert, etc) and put into json
+    if "request cannot be" in output.message.content:
+        print("The generated query was invalid: ", output.message.content)
+    else:
+        json_obj = validate(desc, output.message.content)
+
+    #now that the query is validated, execute it
+    if json_obj is not None:
+        execute(json_obj)
+
 #REPL
-while True:
-    userInput = input("Welcome to the gravity books helpdesk! Please input what you would like to lookup in the database: ").strip()
-    classification = evaluate_Input(userInput)
-    if(classification == 'unsafe'):
-        print("Your input was classified as unsafe. Remember that this is a PROFESSIONAL, LOGGED" \
-        "SYSTEM. Revise your query and try again.")
-    elif(classification == 'unrelated'):
-        print("This is system designed for analysts working with the gravity_books database. Your " \
-        "input was unrelated to this context. Please revise your query and try again.")
-    elif(classification == 'safe'):
-        print("Proceeding with query...")
+if __name__ == "__main__":
+    while True:
+        # add check for privacy?
+        userInput = input("Welcome to the gravity books helpdesk! Please input what you would like to lookup in the database: ").strip()
+        classification = evaluate_Input(userInput)
+        if(classification == 'unsafe'):
+            print("Your input was classified as unsafe. Remember that this is a PROFESSIONAL, LOGGED " \
+            "SYSTEM. Revise your query and try again.")
+        elif(classification == 'unrelated'):
+            print("This is system designed for analysts working with the gravity_books database. Your " \
+            "input was unrelated to this context. Please revise your query and try again.")
+        elif(classification == 'safe'):
+            print("Proceeding with query...")
+            generateSQL(userInput)
