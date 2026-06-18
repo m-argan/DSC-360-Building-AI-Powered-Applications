c336c4bfe07299a7e266fd5d46bf8fa3e06ac473
Madeleine Arganbright <m.arganbright@centre.edu>
2026-06-18 18:30:55 -0400

    Set a larger context to fix bug and changed input command to enter

--- lab04/pandoogle.py
+++ lab04/pandoogle.py
@@ -52,37 +52,44 @@ def docSearch() -> str:
             for elem in top:
                 current = json.loads(chunks[elem])
                 sim = flat[elem]
-                context_block = f"\nSymbol: {current['symbol']}\nSignature: {current['signature']}\nDescription: {current['doc']}\n"
+                context_block = f"\nSymbol: {current['symbol']}\nSignature: {current['signature']}\nDescription: {current['doc'][:1500]}\n"
                 context += "next document: " + context_block + f" with similarity score of {sim}\n"
     return context
 
-# Retrieve user input
-print("Please input your query! (press enter and then ctrl+D to submit)")
-userInput = ''
-try:
-    while True:
-        userInput += input("").strip()
-except EOFError as e:
-    print('Searching...')
-# Generation!
-context = docSearch()
-prompt = f"""
-The user has asked you a question about using pandas.
-Here is a string containing the most relevant panda docs with a symbol, signature, description,
-and a similarity score (higher number is a closer answer). If the context does not contain relevant information, print "Couldn't find an answer, please revise your query and try again".
-Use this context string to answer the question as accurately as possible, respond ONLY in plain text:
-{context}
-Also make sure to include the dataframe symbol that corresponds to the document you are using to answer the question.
-Here is the question: {userInput}.
-""" # formatting done by ChatGPT
-try:
-    print("Generating response...")
-    response = ollama.chat(model = GE_MODEL, messages = [{"role" : "user", "content" : prompt}])  
-    r = response.message.content
-    print(r)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Elapsed time: {elapsed_time:.4f} seconds")
-except ollama.ResponseError as e:
-    print("Error", e.error)
+def queryOllama(userInput):
+    # Generation!
+    context = docSearch()
+    prompt = f"""
+    The user has asked you a question about using pandas.
+    Here is a string containing the most relevant panda docs with a symbol, signature, description,
+    and a similarity score (higher number is a closer answer). If the context does not contain relevant information, print "Couldn't find an answer, please revise your query and try again".
+    Use this context string to answer the question as accurately as possible, respond ONLY in plain text with NO code fences:
+    {context}
+    Also make sure to include the symbol that corresponds to the document you are using to answer the question.
+    Here is the question: {userInput}.
+    """ # formatting done by ChatGPT
+    try:
+        print("Generating response...")
+        # enlarge the context to ensure output
+        opts = {"num_ctx": 131071, "num_predict": 512}
+        response = ollama.chat(model = GE_MODEL, messages = [{"role" : "user", "content" : prompt}], options = opts)  
+        r = response.message.content
+        print(r)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Elapsed time: {elapsed_time:.4f} seconds")
+    except ollama.ResponseError as e:
+        print("Error", e.error)
 
+# Retrieve user input
+print("Please input your query! (enter to submit and /exit to cancel)")
+userInput = ''
+while True:
+    userInput += input("").strip()
+    if '/exit' in userInput:
+        print("Goodbye!")
+        break
+    else:
+        print('Searching...')
+        queryOllama(userInput)
+        break
