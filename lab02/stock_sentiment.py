cc5cac100beade9387910806d153364ea6a0f3f0
Madeleine Arganbright <m.arganbright@centre.edu>
2026-06-18 00:17:19 -0400

    added reprompt in case model responds with more than one word and reformatted confusion matrix

--- lab02/stock_sentiment.py
+++ lab02/stock_sentiment.py
@@ -1,12 +1,11 @@
-import sys
 import pandas as pd
 import sklearn
 from sklearn.metrics import accuracy_score, f1_score
 import ollama
 from sklearn.model_selection import train_test_split
-import numpy as np
+import string
 
-MODEL = "qwen2:0.5b"
+MODEL = "gemma3:1b"
 INPUT_CSV = "data.csv"
 ##OUT_CSV = "compare.csv"
 
@@ -32,30 +31,33 @@ def split() -> None:
         training_data.append(elem)
     trainingStr = ",".join(training_data)
     print("done with getting training data")
+
+    # Only perform sentiment analysis on 100 sentences, for time's sake
     trunctest = x_test[:100]
     response_list = []
+    prog = 0
     for item in trunctest:
-        response = run_analysis(trainingStr, item, response_list)
+        prog +=1
+        response = run_analysis(trainingStr, item, response_list, prog)
 
-    #print("actual : " , first10responses)
-    #print("result : " , response)
-    testexpected = y_test[:10]
+    testexpected = y_test[:100]
     perform_checks(testexpected, response)
-    #print("expected : " , first10responses, "\nresponse : " , response)
     
-
-def run_analysis(training, x, response_list) -> list:
-    #strx = ' '.join(str(val) for val in x)
-    #print("current sentence" , x)
-    prompt = "Here is a list of sentences about the stock market and whether they are positive, negative, or neutral statements: " + training + \
-    "use this training data to run your own sentiment analysis on the following sentence : " + x + " if the tone of the " \
-        "sentence is positive, respond with only the word 'positive'. if it is negative, respond with only the word 'negative'." \
-            "If it is neutral, respond with 'neutral'."
+# Runs a sentiment analysis on sentence x, using the training data list to guide generation
+def run_analysis(training, x, response_list, prog) -> list:
+    prompt = "You are performing a sentiment analysis on remarks about the stock market. Here is a list of sentences about the stock market to use as training data: [" + training + "]. Now, run your own sentiment analysis on the following sentence: " + x + ". if the tone of the " \
+        "sentence is positive, respond with only the word 'positive'. Or, if it is negative, respond with only the word 'negative'. Otherwise, if it is neutral, respond with 'neutral'. Your response should not be more than one word."
     try:
         response = ollama.chat(model = MODEL, messages = [{"role" : "user", "content" : prompt}])  
-        r = response.message.content
-        rr = r.strip().lower()
-        response_list.append(rr)
+        clean_r = response.message.content.strip().lower().translate(str.maketrans('', '', string.punctuation))
+        # check the response is either the word positive, neutral or negative. If not, re-feed into model to correct
+        print("analyzing sentence", prog, "/100")
+        if(clean_r == "positive" or clean_r == "negative" or clean_r == "neutral"):
+            response_list.append(str(clean_r))
+        else:
+            response = ollama.chat(model = MODEL, messages = [{"role" : "user", "content" : "fix this sentence so it ONLY contains the word 'positive', 'negative', or 'neutral' and respond with ONLY that one word:"+clean_r}])  
+            clean_r = response.message.content.strip().lower().translate(str.maketrans('', '', string.punctuation))
+            response_list.append(str(clean_r))
     except ollama.ResponseError as e:
         print("Error", e.error)
 
@@ -72,6 +74,7 @@ def get_expected() -> list:
 def convert_to_int(list) -> list:
     new_list = []
     for i in list:
+        i = i.strip()
         if i == "negative":
             new_list.append(0)
         elif i == "neutral":
@@ -91,19 +94,21 @@ def perform_checks(true, predicted) -> None:
 
     # MAE
     num_true = convert_to_int(true)
+    print(num_true)
     num_pred = convert_to_int(predicted)
+    print(num_pred)
     mae = sklearn.metrics.mean_absolute_error(num_true, num_pred, multioutput='raw_values')
     print("MAE: ", mae)
     
     # confusion matrix
     print("true:")
     con = sklearn.metrics.confusion_matrix(num_true, num_pred)
-    print("confusion matrix: \n", con)
+    print("confusion matrix: \n    neg neu pos", "\n neg", con[0], "\n neu", con[1], "\n pos", con[2])
+    
+    
 
 def main() -> None:
     expected = get_expected()
-
-    
 
     split()
 
